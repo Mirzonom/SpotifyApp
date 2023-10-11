84498426223956fbb01134019a4c6e8513c612cf
Mirzonom <77639035+Mirzonom@users.noreply.github.com>
2023-10-11 23:36:36 +0500

    Добавил зависимости

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -44,4 +45,54 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    // Architectural Components
+    val lifecycle_version = "2.6.2"
+    implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycle_version")
+    implementation ("androidx.lifecycle:lifecycle-livedata-ktx:$lifecycle_version")
+    implementation ("androidx.lifecycle:lifecycle-runtime-ktx:$lifecycle_version")
+
+
+//    implementation ('com.google.firebase:firebase-firestore-ktx:24.8.1')
+
+    //implementation fileTree(org.gradle.internal.impldep.bsh.commands.dir: "libs", include: ["*.jar"])
+
+
+
+    // Lifecycle
+//    implementation "androidx.lifecycle:lifecycle-extensions:2.2.0"
+//    implementation "androidx.lifecycle:lifecycle-livedata-ktx:2.2.0"
+//    implementation "androidx.lifecycle:lifecycle-runtime:2.2.0"
+//    implementation "androidx.lifecycle:lifecycle-runtime-ktx:2.2.0"
+
+    // Coroutines
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.9")
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.3.9")
+
+
+    // Navigation Component
+    val nav_version = "2.7.4"
+    implementation ("androidx.navigation:navigation-fragment-ktx:$nav_version")
+    implementation ("androidx.navigation:navigation-ui-ktx:$nav_version")
+
+    // Glide
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+
+    //Dagger - Hilt
+    implementation("com.google.dagger:dagger-android:2.x")
+    implementation("androidx.hilt:hilt-lifecycle-viewmodel:1.0.0-alpha02")
+
+    // Timber
+    implementation("com.jakewharton.timber:timber:4.7.1")
+
+    // Firebase
+    implementation(platform("com.google.firebase:firebase-bom:32.3.1"))
+    implementation("com.google.firebase:firebase-analytics-ktx")
+    implementation ("com.google.firebase:firebase-auth")
+    implementation ("com.google.firebase:firebase-firestore")
+
+    // ExoPlayer
+    api ("com.google.android.exoplayer:exoplayer-core:2.11.8")
+    api ("com.google.android.exoplayer:exoplayer-ui:2.11.8")
+    api ("com.google.android.exoplayer:extension-mediasession:2.11.8")
 }
